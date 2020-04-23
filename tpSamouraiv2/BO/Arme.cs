c24b941e75937515c77337686905a977484cde74
Sam <samgarrec@gmail.com>
2020-04-23 16:00:39 +0200

    samouraiv2bo

---
+++ tpSamouraiv2/BO/Arme.cs
@@ -0,0 +1,12 @@
+﻿namespace BO
+{
+    public class Arme : Dojo
+    {
+        
+        public string Nom { get; set; }
+        public int? Degats { get; set; }
+
+
+        public Samourai Samourai { get; set; }
+    }
+}
---
+++ tpSamouraiv2/BO/Samourai.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace BO
+{
+    public class Samourai : Dojo
+    {
+
+        public int Force { get; set; }
+        public string Nom { get; set; }
+        public virtual Arme Arme { get; set; }
+        public int Potentiel { get; set; }
+        public virtual List<ArtMartial> ArtsMartiaux { get; set; } = new List<ArtMartial>();
+    }
+}
